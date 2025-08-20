9c4da7c4ca559b85c16afcc93abcbc6bd5aad0a4
Fabiano César <fcfabiano.cesar@gmail.com>
2025-08-20 16:14:31 -0300

    feat(app): configure routes using React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 // -- IMPORTS
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// -- CONSTANTS
+
+const queryClient = new QueryClient();
 
 // -- FUNCTIONS
 
@@ -7,10 +13,33 @@ export default function App(
     )
 {
     return (
-        <>
-            <h1 className="text-3xl font-bold underline text-red-500">
-                Hello world!
-            </h1>
-        </>
+        <QueryClientProvider client={queryClient}>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/ofertas">Ofertas</a></li>
+            </ul>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={
+                        <h1 className="text-3xl font-bold underline text-red-500">
+                            Hello world!
+                        </h1>
+                    }
+                    />
+                    <Route path="/ofertas" element={
+                        <h1 className="text-3xl font-bold underline text-red-500">
+                            Ofertas
+                        </h1>
+                    }
+                    />
+                    <Route path="*" element={
+                        <h1 className="text-3xl font-bold underline text-red-500">
+                            Página não encontrada
+                        </h1>
+                    }
+                    />
+                </Routes>
+            </BrowserRouter>
+        </QueryClientProvider>
         );
 }
