515266672c70e6547e56f233e74989f685f60927
Fabiano César <fcfabiano.cesar@gmail.com>
2025-08-21 17:27:30 -0300

    feat: add Radix UI components (Label, Select, Slider)

---
+++ src/components/ui/label.tsx
@@ -0,0 +1,37 @@
+// -- IMPORTS
+
+import * as React from 'react';
+import * as LabelPrimitive from '@radix-ui/react-label';
+import { cva, type VariantProps } from 'class-variance-authority';
+import { getClassNames } from '@/lib/utils'
+
+// -- CONSTANTS
+
+const labelVariants = cva(
+    'text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
+    );
+
+const Label = React.forwardRef<
+        React.ElementRef<typeof LabelPrimitive.Root>,
+        React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> &
+        VariantProps<typeof labelVariants>
+    >(
+        (
+            {
+                className,
+                ...props
+            },
+            ref
+        ) =>
+        (
+            <LabelPrimitive.Root
+                ref={ ref }
+                className={ getClassNames( labelVariants(), className ) }
+                { ...props }
+            />
+        )
+    );
+
+Label.displayName = LabelPrimitive.Root.displayName;
+
+export { Label }
