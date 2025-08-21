b5136abd0815231844a2f038d026bbf4b982954d
Fabiano César <fcfabiano.cesar@gmail.com>
2025-08-21 11:27:25 -0300

    feat(ui): add shadcn Card component

---
+++ src/components/ui/card.tsx
@@ -0,0 +1,157 @@
+// -- IMPORTS
+
+import * as React from 'react';
+import { getClassNames } from '@/lib/utils';
+
+// -- FUNCTIONS
+
+function Card(
+    {
+        className,
+        ...props
+    }: React.ComponentProps<"div">) {
+    return (
+        <div
+            data-slot="card"
+            className={
+                getClassNames(
+                    'bg-card text-card-foreground flex flex-col gap-6 rounded-xl border py-6 shadow-sm',
+                    className
+                    )
+            }
+            { ...props }
+        />
+        );
+}
+
+// ~~
+
+function CardHeader(
+    {
+        className,
+        ...props
+    }: React.ComponentProps<"div">
+    )
+{
+    return (
+        <div
+            data-slot="card-header"
+            className={
+                getClassNames(
+                    '@container/card-header grid auto-rows-min grid-rows-[auto_auto] items-start gap-1.5 px-6 has-data-[slot=card-action]:grid-cols-[1fr_auto] [.border-b]:pb-6',
+                    className
+                )
+            }
+            { ...props }
+        />
+        );
+}
+
+// ~~
+
+function CardTitle(
+    {
+        className,
+        ...props
+    }: React.ComponentProps<"div">
+    )
+{
+    return (
+        <div
+            data-slot="card-title"
+            className={
+                getClassNames( 'leading-none font-semibold', className )
+            }
+            { ...props }
+        />
+        );
+}
+
+// ~~
+
+function CardDescription(
+    {
+        className,
+        ...props
+    }: React.ComponentProps<"div">
+    )
+{
+    return (
+        <div
+            data-slot="card-description"
+            className={
+                getClassNames( 'text-muted-foreground text-sm', className )
+            }
+            { ...props }
+        />
+        );
+}
+
+// ~~
+
+function CardAction(
+    {
+        className,
+        ...props
+    }: React.ComponentProps<"div">
+    )
+{
+    return (
+        <div
+            data-slot="card-action"
+            className={
+                getClassNames(
+                    'col-start-2 row-span-2 row-start-1 self-start justify-self-end',
+                    className
+                    )
+            }
+            { ...props }
+        />
+        );
+}
+
+// ~~
+
+function CardContent(
+    {
+        className,
+        ...props
+    }: React.ComponentProps<"div">
+    )
+{
+    return (
+        <div
+            data-slot="card-content"
+            className={ getClassNames( 'px-6', className ) }
+            { ...props }
+        />
+        );
+}
+
+// ~~
+
+function CardFooter(
+    {
+        className,
+        ...props
+    }: React.ComponentProps<"div">
+    )
+{
+    return (
+        <div
+            data-slot="card-footer"
+            className={ getClassNames( 'flex items-center px-6 [.border-t]:pt-6', className ) }
+            { ...props }
+        />
+        );
+}
+
+export {
+    Card,
+    CardHeader,
+    CardFooter,
+    CardTitle,
+    CardAction,
+    CardDescription,
+    CardContent,
+}
